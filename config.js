46342e2cfe9333148d6bfb282b508ce49598e27f
Montserrat Djaddah Meillon <montse.djaddah@gmail.com>
2026-05-26 16:26:50 -0600

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,9 @@
 
 const CONFIG = {
   api: {
-    endpoint: "https://rbk7ljo1b0.execute-api.us-east-1.amazonaws.com/prod/vars",
-    pollInterval: 5000, //ms
+    endpoint:        "https://rbk7ljo1b0.execute-api.us-east-1.amazonaws.com/prod/vars",
+    historyEndpoint: "", // set this once the /history Lambda endpoint is ready
+    pollInterval:    5000, //ms
   },
 
   chart: { maxPoints: 50 },
