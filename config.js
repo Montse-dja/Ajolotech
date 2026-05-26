b4eaad982295a1a10c07a300afbf3852f83bc913
Montserrat Djaddah Meillon <montse.djaddah@gmail.com>
2026-05-25 18:12:09 -0600

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@
 const CONFIG = {
   api: {
     endpoint: "https://rbk7ljo1b0.execute-api.us-east-1.amazonaws.com/prod/vars",
-    pollInterval: 5000,
+    pollInterval: 1000,
   },
 
   chart: { maxPoints: 50 },
